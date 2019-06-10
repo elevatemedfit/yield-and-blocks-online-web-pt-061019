04578b3da7206fe426755b9d51810591c6f68d2c
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-10 17:47:53 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -11,3 +11,14 @@ end
 
 hello_t(names)
 # call your method here!
+other_names = ["Ali", "Jasmine", "Persephone"]
+def hello_t(other_names)
+  puts other_names
+  i = 0
+  while i < other_names.length
+    i = i + 1
+end
+other_names
+end
+
+hello_t(other_names)
